6fb7c6f8c83941b148c2748fc271391f7127d61d
Nahor <nahor.j+github@gmail.com>
2023-12-26 18:38:51 -0800

    Refactoring mostly
    + better statistics
    + correctly handle the case where the data starts when switching away from
      DST

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,13 @@ mod error;
 pub use data::*;
 pub use error::SensorError;
 
-use chrono::TimeZone;
 #[cfg(feature = "rayon")]
 use rayon::prelude::*;
 use std::{fs, str::FromStr};
 
 pub fn parse(file: &str) -> Result<Vec<DataPoint>, SensorError> {
+    let start = std::time::Instant::now();
+
     let content = fs::read_to_string(file)?;
     if content.is_empty() {
         println!("empty");
@@ -20,42 +21,100 @@ pub fn parse(file: &str) -> Result<Vec<DataPoint>, SensorError> {
 
     let pos = content
         .find('\n')
-        .ok_or_else(|| SensorError::from("No data in file"))?;
+        .ok_or(SensorError::from("No data in file"))?;
+    let as_celsius = parse_header(&content[..pos])?;
+
+    // TODO(nahor): how to get the timezone from Windows?
+    let tz = chrono_tz::Tz::from_str(&match std::env::var("TZ") {
+        Ok(tz) => {
+            println!("Timezone: {tz:?}");
+            tz
+        }
+        Err(_) => {
+            const DEFAULT: &str = "America/Los_Angeles";
+            println!("Using default timezone: {DEFAULT}");
+            DEFAULT.to_owned()
+        }
+    })?;
+
+    println!("Load: {:.03?}", (std::time::Instant::now() - start));
     let start = std::time::Instant::now();
-    let header = &content[..pos];
-    let content = &content[pos + 1..];
 
-    let as_celsius;
-    if header == r#""Timestamp","Temperature (°C)","Relative Humidity (%)""# {
-        as_celsius = true;
-    } else if header == r#""Timestamp","Temperature (°F)","Relative Humidity (%)""# {
-        as_celsius = false;
-    } else {
-        return Err(format!("Invalid header '{header}'").into());
+    // First pass parses the strings using the data as-is (i.e. pretending it's UTC)
+    let data = first_pass(content, pos + 1, as_celsius)?;
+    let mid = std::time::Instant::now();
+    // Second pass converts the date from "fake UTC" to "real UTC" (UTC + TZ)
+    let data = second_pass(data, tz)?;
+
+    let end = std::time::Instant::now();
+
+    // Statistics
+    {
+        let start = std::time::Instant::now();
+        #[cfg(feature = "rayon")]
+        let _data: Vec<_> = data.par_iter().map(|d| d.clone()).collect();
+        #[cfg(not(feature = "rayon"))]
+        let data: Vec<_> = data.iter().map(|d| d.clone()).collect();
+        let end = std::time::Instant::now();
+        println!(
+            "    (Empty pass: {:.3?} ({:.0} lines/sec))",
+            (end - start),
+            data.len() as f32 / (end - start).as_secs_f32()
+        );
     }
+    println!(
+        "    First pass: {:.3?} ({:.0} lines/sec)",
+        (mid - start),
+        data.len() as f32 / (mid - start).as_secs_f32()
+    );
+    println!(
+        "    Second pass: {:.3?} ({:.0} lines/sec)",
+        (end - mid),
+        data.len() as f32 / (end - mid).as_secs_f32()
+    );
 
-    // TODO(nahor): how to get the timezone from Windows
-    let tz = std::env::var("TZ").unwrap_or("America/Los_Angeles".to_string());
-    println!("Timezone: {tz:?}");
-    let tz = chrono_tz::Tz::from_str(&tz)?;
+    println!(
+        "Total: {:.3?} for {} lines ({:.0} lines/sec)",
+        (end - start),
+        data.len(),
+        data.len() as f32 / (end - start).as_secs_f32()
+    );
 
+    Ok(data)
+}
+
+fn parse_header(header: &str) -> Result<bool, SensorError> {
+    if header == r#""Timestamp","Temperature (°C)","Relative Humidity (%)""# {
+        Ok(true)
+    } else if header == r#""Timestamp","Temperature (°F)","Relative Humidity (%)""# {
+        Ok(false)
+    } else {
+        Err(format!("Invalid header '{header}'").into())
+    }
+}
+
+fn first_pass(
+    content: String,
+    pos: usize,
+    as_celsius: bool,
+) -> Result<Vec<DataPoint>, SensorError> {
     // Seems faster to split first, and only then do the parsing
     // (possible reason: a search for a char is limited by the memory bandwidth
     // and by doing it separately, we avoid trashing the cache?)
-
     #[cfg(feature = "rayon")]
-    let content: Vec<_> = content.par_split('\n').collect();
+    let lines: Vec<_> = content[pos..].par_split('\n').collect();
     #[cfg(feature = "rayon")]
-    let iter = content.par_iter();
+    let iter = lines.into_par_iter();
     #[cfg(not(feature = "rayon"))]
-    let iter = content.split('\n');
+    let lines: Vec<_> = content[pos..].split('\n').collect();
+    #[cfg(not(feature = "rayon"))]
+    let iter = lines.into_iter();
 
-    let data : Result<Vec<_>,_> = iter
-        .map(|line| {
-            parse_line(line, as_celsius)
-        })
-        .enumerate()
-        .filter_map(|(lineno, data)|
+    iter.map(|line| parse_line(line, as_celsius))
+        // .enumerate()
+        // .filter_map(|(lineno, data)|
+        .filter_map(|data| {
+            let lineno = 0;
             // Remove the `Option`, filtering the `None` cases
             // Note that since filter_map expects an `Option` of its own, this
             // is essentially change a `Result<Option<_>,_>` into a `Option<Result<_, _>>`
@@ -63,17 +122,37 @@ pub fn parse(file: &str) -> Result<Vec<DataPoint>, SensorError> {
                 Ok(None) => None,
                 Ok(Some(data)) => Some(Ok(data.clone())),
                 // "+2" because of header+starting at 1
-                Err(err) => Some(Err(SensorError::from((format!("Failed to parse line {}", lineno+2), err)))),
-            })
-        .collect();
-    let data = data?;
+                Err(err) => Some(Err(SensorError::from((
+                    format!("Failed to parse line {}", lineno + 2),
+                    err,
+                )))),
+            }
+        })
+        .collect()
+}
 
-    let extra_data = [DataPoint {
-        datetime: chrono::NaiveDateTime::default().and_utc(),
-        temperature: Celsius::new(0.0),
-        #[cfg(feature = "humidity")]
-        humidity: 0.0,
-    }; 60];
+fn second_pass(
+    data: Vec<DataPoint>,
+    tz: impl chrono::TimeZone + Sync,
+) -> Result<Vec<DataPoint>, SensorError> {
+    // To resolve the ambiguous dates, combine the data points with one shifted
+    // back by 1h, so we have date D and date D-1h. If D-1h is NOT ambiguous,
+    // it means D in the first half of the ambiguous period (still in daylight
+    // saving). If D-1h IS ambiguous, then D is in the second half (with D-1h
+    // in the first half)
+    //
+    // To shift, we process/prepend 60 fake data points before chaining with
+    // the real data.
+    let start_datetime = data[0].datetime.naive_utc();
+    let extra_data: Vec<_> = (1..=60)
+        .rev()
+        .map(|i| DataPoint {
+            datetime: (start_datetime - chrono::Duration::minutes(i)).and_utc(),
+            temperature: Celsius::new(0.0),
+            #[cfg(feature = "humidity")]
+            humidity: 0.0,
+        })
+        .collect();
 
     #[cfg(feature = "rayon")]
     let iter = data
@@ -82,40 +161,31 @@ pub fn parse(file: &str) -> Result<Vec<DataPoint>, SensorError> {
     #[cfg(not(feature = "rayon"))]
     let iter = data.iter().zip(extra_data.iter().chain(data.iter()));
 
-    let data: Result<Vec<_>, _> = iter
-        .map(|(&data, &data_ago)| {
-            let datetime = match tz.from_local_datetime(&data.datetime.naive_utc()) {
-                chrono::LocalResult::None => Err(SensorError::from("Failed to convert date")),
-                chrono::LocalResult::Single(date) => Ok(date),
-                chrono::LocalResult::Ambiguous(min, max) => {
-                    let datetime_ago = tz.from_local_datetime(&data_ago.datetime.naive_utc());
-                    match datetime_ago {
-                        chrono::LocalResult::None => {
-                            Err(SensorError::from("Failed to convert date"))
-                        }
-                        chrono::LocalResult::Single(_) => Ok(min),
-                        chrono::LocalResult::Ambiguous(_, _) => Ok(max),
-                    }
+    iter.map(|(&data, &data_ago)| {
+        let datetime = match tz.from_local_datetime(&data.datetime.naive_utc()) {
+            chrono::LocalResult::None => Err(SensorError::from("Failed to convert date")),
+            chrono::LocalResult::Single(date) => Ok(date),
+            chrono::LocalResult::Ambiguous(min, max) => {
+                // If it wasn't for the parallel processing, we could just
+                // compare with the datetime of the previous item (presumably
+                // properly converted). But with parallelism, the previous item
+                // might be in another thread, and yet to be processed.
+                // Moreover, this only applies to 0.02% of the dates (2 out of
+                // ~8760h/y), ... so yeah, no worth the trouble
+                let datetime_ago = tz.from_local_datetime(&data_ago.datetime.naive_utc());
+                match datetime_ago {
+                    chrono::LocalResult::None => Err(SensorError::from("Failed to convert date")),
+                    chrono::LocalResult::Single(_) => Ok(min),
+                    chrono::LocalResult::Ambiguous(_, _) => Ok(max),
                 }
-            }?;
-            Ok::<DataPoint, SensorError>(DataPoint {
-                datetime: datetime.with_timezone(&chrono::Utc),
-                ..data
-            })
+            }
+        }?;
+        Ok::<DataPoint, SensorError>(DataPoint {
+            datetime: datetime.with_timezone(&chrono::Utc),
+            ..data
         })
-        .collect();
-    let data = data?;
-
-    let end = std::time::Instant::now();
-    let elapsed = end - start;
-    println!("Lines: {}", data.len());
-    println!(
-        "Speed: {:.0} lines/sec (time: {:?})",
-        data.len() as f32 / elapsed.as_secs_f32(),
-        elapsed
-    );
-
-    Ok(data)
+    })
+    .collect()
 }
 
 fn parse_line(line: &str, as_celsius: bool) -> Result<Option<DataPoint>, SensorError> {
@@ -132,7 +202,7 @@ fn parse_line(line: &str, as_celsius: bool) -> Result<Option<DataPoint>, SensorE
         .next()
         .ok_or(SensorError::from("Missing temperature"))?
         .trim_matches('"')
-        .parse::<f64>()
+        .parse()
         .or_else(|err| Err(SensorError::from(("Invalid temperature", err))))?;
     let temperature = if as_celsius {
         Celsius::new(temperature)
