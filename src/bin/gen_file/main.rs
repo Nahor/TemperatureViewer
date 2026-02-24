a122cfbdb5571d1953d2ebbb5a2f1ae4e3e44439
Nahor <nahor.j+github@gmail.com>
2026-02-24 14:07:49 -0800

    Move progress bar code in a module

--- src/bin/gen_file/main.rs
+++ src/bin/gen_file/main.rs
@@ -4,14 +4,16 @@ use std::{
     error::Error,
     io::{BufWriter, Write},
     sync::{Arc, Condvar, Mutex, mpsc},
-    time::{Duration, Instant},
-    vec,
 };
 
 use jiff::{SignedDuration, tz::TimeZone};
 #[cfg(feature = "rayon")]
 use rayon::prelude::*;
 use sensor::DATAPOINT_EPOCH;
+
+use crate::progress::Progress;
+
+mod progress;
 
 #[allow(unused)]
 const MAX_THREADS: usize = usize::MAX;
@@ -36,101 +38,6 @@ fn size_format(v: usize) -> String {
         format!("{v:.2}KB", v = v / KB)
     } else {
         format!("{v}B")
-    }
-}
-
-struct Progress {
-    max_value: usize,
-    value: usize,
-
-    width: usize,
-    mapped_progress: usize,
-
-    start: Instant,
-    last_update: Instant,
-    speed: Option<f64>,
-}
-impl Progress {
-    const PROGRESS_CHARS: &'static [char] = &['▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'];
-    fn new(max_value: usize, width: usize) -> Self {
-        Self {
-            max_value,
-            value: 0,
-
-            width,
-            mapped_progress: 0,
-
-            start: Instant::now(),
-            last_update: Instant::now() - Duration::from_secs(1),
-            speed: None,
-        }
-    }
-    fn inc(&mut self, inc: usize) {
-        self.set_value(self.value + inc);
-        self.draw();
-    }
-    fn set_value(&mut self, v: usize) {
-        self.value = v.min(self.max_value);
-        self.draw();
-    }
-    fn finish(&mut self) {
-        self.value = self.max_value;
-        self.draw();
-    }
-
-    fn draw(&mut self) {
-        let virtual_width = self.width * Self::PROGRESS_CHARS.len();
-        let progress = self.value * virtual_width / self.max_value;
-
-        // Skip the refresh if there is no visible progress and not enough time has passed
-        let time = Instant::now();
-        if (time < self.last_update + Duration::from_millis(100))
-            && (progress == self.mapped_progress)
-        {
-            return;
-        }
-        self.last_update = time;
-        self.mapped_progress = progress;
-
-        // Generate the progress bar
-        let full_blocks = progress / Self::PROGRESS_CHARS.len();
-        let partial_block_idx = progress % Self::PROGRESS_CHARS.len();
-        let mut vec: Vec<char> = vec![*Self::PROGRESS_CHARS.last().unwrap(); full_blocks];
-        if partial_block_idx > 0 {
-            vec.push(Self::PROGRESS_CHARS[partial_block_idx - 1]);
-        }
-        let progress_str: String = vec.iter().collect();
-
-        // Estimate the speed
-        // Do it only if we have enough data to mean something
-        // And use a running average to avoid too much variance early on
-        let elapsed = (Instant::now() - self.start).as_secs_f64();
-        if elapsed > Duration::from_secs(1).as_secs_f64() {
-            self.speed = match self.speed {
-                Some(speed) => Some(speed * 0.9 + self.value as f64 / elapsed * 0.1),
-                None => Some(self.value as f64 / elapsed),
-            }
-        }
-
-        // Compute the ETA
-        let eta = match self.speed {
-            Some(speed) => {
-                let eta = f64::ceil((self.max_value - self.value) as f64 / speed) as usize;
-                format!("{}:{:02}", eta / 60, eta % 60)
-            }
-            None => "unk".to_owned(),
-        };
-
-        print!(
-            //"\r[{:>2}:{:02}] [{:░<50}] {:>3}% ({})",
-            "\r[{:>2}:{:02}] [{: <50}] {:>3}% ({})",
-            elapsed as usize / 60,
-            elapsed as usize % 60,
-            progress_str,
-            self.value * 100 / self.max_value,
-            eta
-        );
-        std::io::stdout().flush().unwrap();
     }
 }
 
